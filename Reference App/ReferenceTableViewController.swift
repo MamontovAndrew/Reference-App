f0128c699d0b2be78f8969479db5be8b9883465a
Programming <programming@macbook-pro-programming-6.local>
2019-03-02 16:25:38 +0300

    Initial commit

---
+++ Reference App/ReferenceTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ReferenceTableViewController.swift
+//  Reference App
+//
+//  Created by Programming on 02.03.2019.
+//  Copyright © 2019 Programming. All rights reserved.
+//
+
+import UIKit
+
+class ReferenceTableViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10000
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        cell.textLabel?.text = "Row: \(indexPath.row)"
+        
+        return cell
+    }
+}
